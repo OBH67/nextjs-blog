a17ba5ccc5a49bf4196c440cc0ff0e055cca5c23
OBH67 <omarbalanzar113@gmail.com>
2021-02-09 19:51:56 -0600

    version precommit

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import Layout, { siteTitle } from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
 import { getSortedPostsData } from '../lib/posts'
+import Link from 'next/link'
 
 export default function Home({ allPostsData }) {
   return (
@@ -11,6 +12,13 @@ export default function Home({ allPostsData }) {
       </Head>
       <section className={utilStyles.headingMd}>
       <h1>NextJs App</h1>
+      <Link href="/posts/ssg-ssr">
+      <a> Ir a LocalFile Ssg-Sssr </a>
+      </Link>
+      <br />
+      <Link href="posts/pre-rendering">
+      <a> Ir a LocalGile Pre-Rendering </a>
+      </Link>
       </section>
        <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
